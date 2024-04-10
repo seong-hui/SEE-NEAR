d09812019f0724a44a8c27dbcbc56ce8fe2b9068
moonseonghui <msh9980@naver.com>
2024-04-10 14:55:21 +0900

    [FE] FEAT : 대화하기 버튼 클릭 시 해당 영역 커지는 효과#9

--- front/src/pages/MainPage.tsx
+++ front/src/pages/MainPage.tsx
@@ -5,6 +5,7 @@ import { currentTimer } from "@/utils/timerUtils";
 
 const MainPage = () => {
   const [timer, setTimer] = useState("0000년 00월 00일 00시 00분 00초");
+  const [isChatActive, setIsChatActive] = useState(false);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -14,24 +15,30 @@ const MainPage = () => {
     return () => clearInterval(intervalId);
   }, []);
 
+  const onClickChatBtn = () => {
+    setIsChatActive(!isChatActive);
+  };
+
   return (
     <MainPageStyled>
       <CurrentTimeStyled>{timer}</CurrentTimeStyled>
       <ConstantBoxWapped>
-        <ContentBoxStyled>
+        <ContentBoxStyled onClick={onClickChatBtn} active={isChatActive}>
           <LogoImgStyled src={LogoImg} />
           <ContentTitle>대화하기</ContentTitle>
         </ContentBoxStyled>
-        <ContentBoxStyled>
-          <ScheduleTitle>오늘의 일정</ScheduleTitle>
-        </ContentBoxStyled>
+        {!isChatActive && (
+          <ContentBoxStyled>
+            <ScheduleTitle>오늘의 일정</ScheduleTitle>
+          </ContentBoxStyled>
+        )}
       </ConstantBoxWapped>
     </MainPageStyled>
   );
 };
 
 const MainPageStyled = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-color: var(--point-color);
   display: flex;
@@ -47,11 +54,12 @@ const CurrentTimeStyled = styled.div`
 
 const ConstantBoxWapped = styled.div`
   display: flex;
+  width: 90%;
 `;
 
-const ContentBoxStyled = styled.div`
-  width: 650px;
-  height: 650px;
+const ContentBoxStyled = styled.div.withConfig({
+  shouldForwardProp: (prop) => prop !== "active",
+})<{ active?: boolean }>`
   background-color: var(--sub-color);
   border-radius: 20px;
   margin: 30px;
@@ -61,11 +69,26 @@ const ContentBoxStyled = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  cursor: pointer;
+  transition: width 0.4s ease;
+  ${(props) =>
+    props.active &&
+    `
+    width: 90%;
+    height: 600px;
+  `}
+
+  ${(props) =>
+    !props.active &&
+    `
+    width: 50%;
+    height: 600px;
+  `}
 `;
 
 const LogoImgStyled = styled.img`
-  width: 600px;
-  height: 600px;
+  width: 80%;
+  height: 80%;
 `;
 
 const ScheduleTitle = styled.div`
