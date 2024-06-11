497a827036f50b15941cc72c1040cfb646e71c9e
pjs <wotmd36963@gmail.com>
2024-06-11 22:39:00 +0900

    [FE] FEAT : events chatbot

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('auth/', include("authentication.urls")),
     path('events/', include("events.urls")),
     path('conv/', include("conversation.urls")),
-    path('api/', include('chatbot.urls')),
+    path('chat/', include('chatbot.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
     
